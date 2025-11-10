7af70201f184cd794a8db597bd6e6fedb7829faf
mohanram005 <136675711+mohanram005@users.noreply.github.com>
2025-11-10 08:19:06 +0530

    Update and rename array_problems to array_problems.c

--- DAY 5/array_problems.c
+++ DAY 5/array_problems.c
@@ -24,3 +24,23 @@ int main() {
 
     return 0;
 }
+
+
+
+/Reverse an array
+
+//Print the elements of an array in reverse order without changing the original array.
+#include <stdio.h>
+
+int main() {
+    
+    int a[5] = {1,2,3,4,5};
+    
+    for(int i = 4;i >= 0;i--){
+        
+        printf("the revese order is %d\n",a[i]);
+        
+    }
+
+    return 0;
+}
